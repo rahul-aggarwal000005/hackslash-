ac9ea1ef1e9830675c9587fcfd23f41d1eb1d397
Rahul <rahul.aggarwal000005@gmail.com>
2020-12-13 10:16:09 +0530

    main screen and added functionalities

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParse.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+let successMessage = ''
 
+
+mongoose.connect("mongodb://localhost:27017/FeedbackDB", { useNewUrlParser: true });
+
+const feedbackSchema = {
+    name: String,
+    email: String,
+    feedback: String
+}
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+const rahul = new Feedback({
+    name: "Rahul",
+    email: "rahul.aggarwal000005@gmail.com",
+    feedback: "Best Services Provided"
+});
+
+const megha = new Feedback({
+    name: "Megha",
+    email: "meghaislovely01@gmail.com",
+    feedback: "Impressive UI"
+});
+
+const vaibhavi = new Feedback({
+    name: "Vaibhavi",
+    email: "jhavai2201@gmail.com",
+    feedback: "Beautiful and smooth working website"
+});
+
+const defaultFeedbacks = [rahul, megha, vaibhavi];
 
 
 
@@ -18,8 +49,12 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 });
 
+app.get('/land', function(req, res) {
+    res.redirect('/');
+});
+
 app.get('/home', function(req, res) {
-    res.render('main.ejs');
+    res.render('main.ejs', { success: successMessage });
 });
 
 app.get("/main", function(req, res) {
@@ -34,31 +69,48 @@ app.get("/palace", function(req, res) {
     res.render('palace.ejs');
 });
 
+app.get('/feedback', function(req, res) {
+
+
+    Feedback.find({}, function(err, foundFeedback) {
+        if (foundFeedback.length === 0) {
+            Feedback.insertMany(defaultFeedbacks, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully Added");
+                }
+            });
+            res.redirect('/feedback');
+        } else {
+            console.log("loggest your entry");
+            res.render('main', { success: successMessage });
+        }
+    });
+});
 
 /* Post Request */
 
-app.post("/signup", function(req, res) {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const password = req.body.password;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    if (firstName.length == 0 || lastName.length == 0 || password.length <= 8 || email.length == 0) {
-        console.log("Please Enter the required Boxes");
-        res.redirect("/signup");
+app.post('/feedback', function(req, res) {
+    const useremail = req.body.userEmail;
+    const username = req.body.userName;
+    const userfeedback = req.body.feedback;
+    if (useremail == '' || username == '' || userfeedback == '') {
+        success = '';
     } else {
-        console.log("First Name : " + firstName + "\n Last Name : " + lastName + "\n Email : " + email + "\n Password : " + password);
-        res.redirect("/");
+
+        const feedbackItem = new Feedback({
+            name: username,
+            email: useremail,
+            feedback: userfeedback
+        });
+        feedbackItem.save();
+        console.log("Your data is saved");
+        successMessage = 'Thankyou for the feedback';
+        res.redirect('/home');
     }
 });
 
-
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log("Username : " + username + "\n Password : " + password);
-    res.redirect("/main");
-});
 
 
 
